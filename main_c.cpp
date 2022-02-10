4f7498f79e82c2c10fcde997a219e9a74d2cf2ce
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2022-02-11 00:43:11 +0100

    Rename main.cpp to main_c.cpp

--- main_c.cpp
+++ main_c.cpp
@@ -11,16 +11,11 @@
 #include"drawing_node.h"
 #include"glstring_topic.h"
 #include"game_logic_topic.h"
-//////////////////////////
-
-
-
-
-
+/
 
 int main(int argc, char** argv) {
-	//initialize mode and open a windows in upper left corner of screen
-	//Windows tittle is name of program
+	
+	//-- creating window
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -28,13 +23,11 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Car Game");
 	//start:
-
 	glutDisplayFunc(display);
 	init();
+	// keyboard control
 	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(keyboardown);
-
 	glutTimerFunc(2000, update, 0); //Add a timer
-
 	glutMainLoop();
 }
